6a95951c0f4cffe51748422ca6ec84f64eee2d2c
sergioqueber <316341@via.dk>
2023-05-25 17:26:30 +0200

    update

---
+++ Solarex/src/view/OverviewController.java
@@ -0,0 +1,19 @@
+package view;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Region;
+import javafx.scene.control.Alert.AlertType;
+
+import java.util.Optional;
+
+public class OverviewController
+{
+
+}
