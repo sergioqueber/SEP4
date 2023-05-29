a4e6d3130c3f484242eaa58cf99b26d9acbebad8
EarthOddity <316353@via.dk>
2023-05-29 16:14:42 +0200

    cositas

---
+++ Solarex/src/view/ManagePanelsController.java
@@ -0,0 +1,83 @@
+package view;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+
+public class ManagePanelsController
+{
+  @FXML
+  private Button addNewButton;
+
+  @FXML
+  private Button editButton;
+
+  @FXML
+  private Label pvLabel;
+
+  @FXML
+  private TableColumn<?, ?> pvLocationC;
+
+  @FXML
+  private TableColumn<?, ?> pvManufacturerC;
+
+  @FXML
+  private TableView<?> pvPanelTable;
+
+  @FXML
+  private TableColumn<?, ?> pvSNC;
+
+  @FXML
+  private Button pvSearch;
+
+  @FXML
+  private TableColumn<?, ?> pvStatusC;
+
+  @FXML
+  private TextField pvText;
+
+  @FXML
+  private TableColumn<?, ?> pvTypeC;
+
+  @FXML
+  private Button removeButton;
+
+  @FXML
+  private Button saveButton;
+
+  @FXML
+  private Label tpLabel;
+
+  @FXML
+  private TableColumn<?, ?> tpLocationC;
+
+  @FXML
+  private TableColumn<?, ?> tpManufacturerC;
+
+  @FXML
+  private TableView<?> tpPanelTable;
+
+  @FXML
+  private TableColumn<?, ?> tpSNC;
+
+  @FXML
+  private Button tpSearch;
+
+  @FXML
+  private TableColumn<?, ?> tpStatusC;
+
+  @FXML
+  private TextField tpText;
+
+  @FXML
+  private TableColumn<?, ?> tpTypeC;
+
+
+
+
+
+}
+
